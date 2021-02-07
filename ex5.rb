194ef237ba19f48d45466fcf59c570f688dca81e
Bojan <boj.bozic@gmail.com>
2021-02-07 01:37:06 +0000

    existing examples added

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Bojan Bozic'
+my_age = 37 # not a lie in 2020
+my_height = 187 # cm
+my_weight = 88 # kg
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
