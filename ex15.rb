314c9607f3352644d74203fe4ae50f93e3402b61
Bojan <boj.bozic@gmail.com>
2021-02-10 02:13:53 +0000

    ex15 updated

--- ex15.rb
+++ ex15.rb
@@ -4,9 +4,11 @@ txt = open(filename)
 
 puts "Here's your file #{filename}:"
 print txt.read
+txt.close
 
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
 print txt_again.read
+txt_again.close
