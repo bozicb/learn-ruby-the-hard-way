5ae41452c3ca077129dfb11f37aa74638fecf335
Bojan <boj.bozic@gmail.com>
2021-02-10 01:29:32 +0000

    new example

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+size = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So you are #{age} years old, #{size} cm tall, and #{weight} kg heavy."
